ef3198575e6da5d2f41dd5ffffe1372f967a47a1
wenjie <softwarecheng@gmail.com>
2018-03-29 12:52:44 +0800

    To add a  mechanism that mail configuration for register

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -234,7 +234,8 @@ authenticationController.login = function (req, res, next) {
 			var requireEmailConfirmation = parseInt(meta.config.requireEmailConfirmation, 10) === 1;
 			var isEmailConfirmed = parseInt(userData['email:confirmed'], 10) === 1;
 			if (!isEmailConfirmed && requireEmailConfirmation) {
-				helpers.noScriptErrors(req, res, "require confirm link in your mail!", 500);
+				var err = '[[error:wrong-login-email-confirm]]';
+				helpers.noScriptErrors(req, res, err, 500);
 			} else if (req.body.username && utils.isEmailValid(req.body.username) && loginWith.indexOf('email') !== -1) {
 				async.waterfall([
 					function (next) {
