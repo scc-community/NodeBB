1809aad554d44243d3835e208d71e02c55ec8abb
wenjie <softwarecheng@gmail.com>
2018-05-02 01:59:25 +0800

    sql data layer and user business layer

--- src/database/mysql/schema.js
+++ src/database/mysql/schema.js
@@ -5,14 +5,30 @@ var tableModels = [
 		name: 'users',
 		idFieldName: 'id',
 	},
+	{
+		name: 'reward_types',
+		idFieldName: 'id',
+	},
+	{
+		name: 'post_rewards',
+		idFieldName: 'id',
+	},
+	{
+		name: 'manual_rewards',
+		idFieldName: 'id',
+	},
+	{
+		name: 'txs',
+		idFieldName: 'id',
+	},
 ];
 var schema = module.exports;
 
 schema.init = function (mysqlClient) {
-	schema.initModeal(mysqlClient);
+	schema.initModel(mysqlClient);
 };
 
-schema.initModeal = function (mysqlClient) {
+schema.initModel = function (mysqlClient) {
 	tableModels.forEach(function (model) {
 		mysqlClient.add(model);
 	});
