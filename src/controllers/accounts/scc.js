9789e4c6ded9517660f0451146a8cca7ea9eb429
ASCE1885 <380283392@qq.com>
2018-05-03 08:49:46 +0800

    add user scc partially

---
+++ src/controllers/accounts/scc.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var async = require('async');
+
+var db = require('../../database');
+var user = require('../../user');
+var meta = require('../../meta');
+var plugins = require('../../plugins');
+var helpers = require('../helpers');
+var groups = require('../../groups');
+var accountHelpers = require('./helpers');
+var privileges = require('../../privileges');
+var file = require('../../file');
+
+var sccController = module.exports;
+
+sccController.get = function (req, res, callback) {
+    
+
+};
