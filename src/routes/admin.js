40a0a4dbd404f3e09b8cf1e5de8a52384def8800
顾浩鑫 <guhaoxin520@pingan.com.cn>
2018-05-04 13:55:12 +0800

    admin-user add scc details page(data still needs to fill)

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -66,7 +66,7 @@ function addRoutes(router, middleware, controllers) {
 	router.get('/manage/users/not-validated', middlewares, controllers.admin.users.notValidated);
 	router.get('/manage/users/no-posts', middlewares, controllers.admin.users.noPosts);
 	router.get('/manage/users/top-posters', middlewares, controllers.admin.users.topPosters);
-	router.get('/manage/users/most-reputation', middlewares, controllers.admin.users.mostReputaion);
+	router.get('/manage/users/most-scc', middlewares, controllers.admin.users.mostScc);
 	router.get('/manage/users/inactive', middlewares, controllers.admin.users.inactive);
 	router.get('/manage/users/flagged', middlewares, controllers.admin.users.flagged);
 	router.get('/manage/users/banned', middlewares, controllers.admin.users.banned);
