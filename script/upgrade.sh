ced6a23fed8f993f09682e530b7ef221a40857d4
wenjie <softwarecheng@gmail.com>
2018-05-18 15:42:12 +0800

    fix upgrade script

---
+++ script/upgrade.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ev
+
+node clean0.js
+node userInvitation1.js
+node user2.js
+node invitationuid3.js
+node userscctoken4.js
+node buildTopicsReward5.js
+node user2sql6.js
+node buildTxForInit7.js
+node users_token_zset.js
