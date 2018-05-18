d3a4d4802a3851ed1c04477506d9333682572d9c
bertrand <weigan.huang@gmail.com>
2018-05-18 15:10:38 +0800

    fix bug

--- script/release_v2.0.1.sh
+++ script/release_v2.0.1.sh
@@ -7,6 +7,6 @@ node user2.js
 node invitationuid3.js
 node userscctoken4.js
 node buildTopicsReward5.js
-node users_token_zset.js
 node user2sql6.js
 node buildTxForInit7.js
+node users_token_zset.js
