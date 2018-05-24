8978f738ab12a45926a93f99c826798657433774
wenjie <softwarecheng@gmail.com>
2018-05-24 16:24:25 +0800

    update sql script

--- script/20180524/upgrade.sh
+++ script/20180524/upgrade.sh
@@ -3,7 +3,7 @@ set -ev
 
 curdir=$(cd "$(dirname "$0")"; pwd)
 echo $curdir
-mysql -hlocalhost -uroot -p < $curdir/update.sql
+mysql -hlocalhost -uscc -p scc< $curdir/update.sql
 node $curdir/fix-rewardcheck.js
 redis-cli hincrby user:18 scctoken 200
 redis-cli hincrby user:4 scctoken 10
